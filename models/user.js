06ee51b4e0d82d3480b5836e4721d4d076700921
DYuHLin <damianver2000@gmail.com>
2024-03-06 15:12:15 +0200

    Made changes to the user model and the homepage.

--- models/user.js
+++ models/user.js
@@ -5,7 +5,9 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
     name: {type: String, required: true},
     password: {type: String, required: true},
-    member: {type: Boolean, default: false}
+    Username: {type: String, required: true},
+    admin: {type: Boolean, default: false},
+    member: {type: Boolean, default: false},
 });
 
 module.exports = mongoose.model("Users", userSchema);
